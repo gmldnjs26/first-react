0ca527f73fe670e7f46118ef5c30a330cc316d30
gmldnjs26 <imleesky265@gmail.com>
2021-12-04 18:59:50 +0900

    chore: chore: ref onKeyPress

--- src/components/ContactCreate.js
+++ src/components/ContactCreate.js
@@ -9,13 +9,19 @@ export default class ContactCreate extends React.Component {
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleClick = this.handleClick.bind(this)
+    this.handleKeyPress = this.handleKeyPress(this)
   }
   handleChange(e) {
     let nextState = {};
     nextState[e.target.name] = e.target.value
     this.setState(nextState)
   }
-  handleClick(e) {
+  handleKeyPress(e) {
+    if(e.charCode === 13) {
+      this.handleClick()
+    }
+  }
+  handleClick() {
     const contact = {
       name: this.state.name,
       phone: this.state.phone
@@ -25,15 +31,15 @@ export default class ContactCreate extends React.Component {
       name: '',
       phone: ''
     })
+    this.nameInput.focus();
   }
-
   render() {
     return(
       <div>
         <h2>Create Component</h2>
         <p>
-          <input type="text" name="name" placeholder="name" value={this.state.name} onInput={this.handleChange} />
-          <input type="text" name="phone" placeholder="phone" value={this.state.phone} onInput={this.handleChange}/>
+          <input ref={(ref) => { this.nameInput = ref }} type="text" name="name" placeholder="name" value={this.state.name} onInput={this.handleChange} />
+          <input type="text" name="phone" placeholder="phone" value={this.state.phone} onInput={this.handleChange} onKeyPress={this.handleKeyPress}/>
         </p>
         <button onClick={this.handleClick}>Create</button>
       </div>
